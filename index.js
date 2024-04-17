604c938f1bc161a0a073e401baaee51f6d5dd224
Renan Augusto <renancandido82@outlook.com>
2024-04-17 13:03:55 -0300

    Biblioteca Virtual

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ import path from 'path';
 const app = express();
 
 app.get('/', (req, res) => {
-    const indexPath = path.resolve('index.html');
+    const indexPath = path.resolve(process.cwd(), 'index.html');
     res.sendFile(indexPath);
 });
 app.get('/css', (req, res) => {
-    const indexPath = path.resolve('/CSS/index.css');
+    const indexPath = path.resolve(process.cwd(), 'CSS/index.css');
     res.sendFile(indexPath);
 });
 app.get('/js', (req, res) => {
-    const indexPath = path.resolve('/JS/index.js');
+    const indexPath = path.resolve(process.cwd(), '/JS/index.js');
     res.sendFile(indexPath);
 });
 app.get('/LIVRO/teste', (req, res) => {
-    const indexPath = path.resolve('/LIVRO/teste.html');
+    const indexPath = path.resolve(process.cwd(), '/LIVRO/teste.html');
     res.sendFile(indexPath);
 });
 app.get('/livrocss', (req, res) => {
-    const indexPath = path.resolve('/CSS/livro.css');
+    const indexPath = path.resolve(process.cwd(), '/CSS/livro.css');
     res.sendFile(indexPath);
 });
 app.get('/turn', (req, res) => {
-    const indexPath = path.resolve('/JS/turn.js');
+    const indexPath = path.resolve(process.cwd(), '/JS/turn.js');
     res.sendFile(indexPath);
 });
 app.listen(3000, () => {
