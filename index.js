aa27247097ce0ccd4a61157e9b05fdef00619a93
Renan Augusto <renancandido82@outlook.com>
2024-04-17 12:59:28 -0300

    Biblioteca Virtual

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ app.get('/', (req, res) => {
     res.sendFile(indexPath);
 });
 app.get('/css', (req, res) => {
-    const indexPath = path.resolve('CSS/index.css');
+    const indexPath = path.resolve('/CSS/index.css');
     res.sendFile(indexPath);
 });
 app.get('/js', (req, res) => {
-    const indexPath = path.resolve('JS/index.js');
+    const indexPath = path.resolve('/JS/index.js');
     res.sendFile(indexPath);
 });
 app.get('/LIVRO/teste', (req, res) => {
-    const indexPath = path.resolve('LIVRO/teste.html');
+    const indexPath = path.resolve('/LIVRO/teste.html');
     res.sendFile(indexPath);
 });
 app.get('/livrocss', (req, res) => {
-    const indexPath = path.resolve('CSS/livro.css');
+    const indexPath = path.resolve('/CSS/livro.css');
     res.sendFile(indexPath);
 });
 app.get('/turn', (req, res) => {
-    const indexPath = path.resolve('JS/turn.js');
+    const indexPath = path.resolve('/JS/turn.js');
     res.sendFile(indexPath);
 });
 app.listen(3000, () => {
