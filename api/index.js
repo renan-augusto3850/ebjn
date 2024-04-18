db560f8506814b82147862be865cb9da93a00d0c
Renan Augusto <renancandido82@outlook.com>
2024-04-18 20:24:09 -0300

    Biblioteca Virtual - WPA support and Subscribe page.

--- api/index.js
+++ api/index.js
@@ -7,12 +7,14 @@ app.get('/', (req, res) => {
     const indexPath = path.resolve(process.cwd(), 'index.html');
     res.sendFile(indexPath);
 });
-app.get('/css', (req, res) => {
-    const indexPath = path.resolve(process.cwd(), 'CSS/index.css');
+app.get('/css/:stylesheet', (req, res) => {
+    const stylesheet = req.params.stylesheet;
+    const indexPath = path.resolve(process.cwd(), `CSS/${stylesheet}.css`);
     res.sendFile(indexPath);
 });
-app.get('/js', (req, res) => {
-    const indexPath = path.resolve(process.cwd(), 'JS/index.js');
+app.get('/js/:script', (req, res) => {
+    const script = req.params.script;
+    const indexPath = path.resolve(process.cwd(), `JS/${script}.js`);
     res.sendFile(indexPath);
 });
 app.get('/LIVRO/:nome_do_livro', (req, res) => {
@@ -20,8 +22,16 @@ app.get('/LIVRO/:nome_do_livro', (req, res) => {
     const indexPath = path.resolve(process.cwd(), `LIVRO/${livro}.html`);
     res.sendFile(indexPath);
 });
-app.get('/turn', (req, res) => {
-    const indexPath = path.resolve(process.cwd(), 'JS/turn.js');
+app.get('/cadastro', (req, res) => {
+    const indexPath = path.resolve(process.cwd(), 'cadastro.html');
+    res.sendFile(indexPath);
+});
+app.get('/manifest', (req, res) => {
+    const indexPath = path.resolve(process.cwd(), 'MANIFEST/manifest.json');
+    res.sendFile(indexPath);
+});
+app.get('/icon', (req, res) => {
+    const indexPath = path.resolve(process.cwd(), 'ICON/icon.png');
     res.sendFile(indexPath);
 });
 app.listen(3000, () => {
