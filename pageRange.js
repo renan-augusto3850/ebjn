9f50aebff4b7531077c2815de4a3a47b07dfe9fe
Renan Augusto <renancandido82@outlook.com>
2024-06-06 12:24:46 -0300

    EBJN - Remove one book.

--- pageRange.js
+++ pageRange.js
@@ -1,11 +1,39 @@
 
-var totalPages = 292;
-var wordsPerPage = 300;
-var ppm = 150;
+export default class pageRange {
+    #dataParaMilissegundos(data) {
+        return new Date(data).getTime();
+    }
 
-var minutes = totalPages * wordsPerPage / ppm;
+    #calcularNovaMedia(milissegundosMediaAtual, milissegundosNovaData, pesoAtual = 1, pesoNovo = 1) {
+        return ((milissegundosMediaAtual * pesoAtual) + (milissegundosNovaData * pesoNovo)) / (pesoAtual + pesoNovo);
+    }
 
-const hours = Math.floor(minutes / 60);
-const restMinutes = minutes % 60;
+    #calcularPontos(diferencaEmDias) {
+        if (diferencaEmDias === 0) {
+            return 0.5;
+        } else if (Math.abs(diferencaEmDias) <= 5) {
+            return 0.4;
+        } else if (Math.abs(diferencaEmDias) <= 9) {
+            return 0.3;
+        } else {
+            return 0.1;
+        }
+    }
 
-console.log(hours, restMinutes);
+    gerarPontuação(mediaLeituraData, novaDataTermino) {
+
+        const milissegundosMediaLeitura = this.#dataParaMilissegundos(mediaLeituraData);
+        const milissegundosNovaData = this.#dataParaMilissegundos(novaDataTermino);
+        const novaMediaMilissegundos = this.#calcularNovaMedia(milissegundosMediaLeitura, milissegundosNovaData);
+        const novaMediaData = new Date(novaMediaMilissegundos).toISOString().split('T')[0];
+
+        console.log('Nova média de leitura (data):', novaMediaData);
+
+        const diferencaEmDias = (milissegundosNovaData - milissegundosMediaLeitura) / (1000 * 60 * 60 * 24);
+
+        const pontos = this.#calcularPontos(diferencaEmDias);
+        console.log('Pontos:', pontos.toFixed(1));
+    }
+}
+const range = new pageRange();
+range.gerarPontuação('2024-05-30', '2024-05-25');
